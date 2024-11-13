b47b3f86935b7c8628574bf4c81aa68f6420bc22
xianzhuoliu <xianzhuoliu@gmail.com>
2024-11-14 01:40:40 +0800

    精简拍照文件名路径格式

--- packages/renderer/src/store/Hardware/index.ts
+++ packages/renderer/src/store/Hardware/index.ts
@@ -17,11 +17,11 @@ const GlobalRef = ref({
     methods: {
         getCurrenDataInfo(){
             const now = new Date()
-            const formattedDateTime = `${now.getFullYear()}-${String(
+            const formattedDateTime = `${now.getFullYear()}${String(
                 now.getMonth() + 1
-            ).padStart(2, "0")}-${String(now.getDate()).padStart(2, "0")}_${String(
+            ).padStart(2, "0")}${String(now.getDate()).padStart(2, "0")}-${String(
                 now.getHours()
-            ).padStart(2, "0")}-${String(now.getMinutes()).padStart(2, "0")}-${String(
+            ).padStart(2, "0")}${String(now.getMinutes()).padStart(2, "0")}${String(
                 now.getSeconds()
             ).padStart(2, "0")}`;
             const dir = path.join(Global.dataRootDir, formattedDateTime)
