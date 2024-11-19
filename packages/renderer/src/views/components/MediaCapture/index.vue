7edbc105f21a5ab1430adb62af03f38693a7b327
liuxianzhuo <liuxianzhuo@lynxiv.com>
2024-11-19 10:26:15 +0800

    更正视频录制消息类型

--- packages/renderer/src/views/components/MediaCapture/index.vue
+++ packages/renderer/src/views/components/MediaCapture/index.vue
@@ -11,9 +11,9 @@
         <div class="workspace dashed-border">
             <div class="media-entry">
                 <p>添加媒体</p>
-                <span style="margin-bottom: 30px"
-                >通过SwifCam添加媒体,或者将您的媒体拖放到此页面的任何地方</span
-                >
+                <span style="margin-bottom: 30px">
+                    通过SwifCam添加媒体,或者将您的媒体拖放到此页面的任何地方
+                </span>
                 <ObButton
                     text="通过 SwifCam 添加媒体"
                     icon="icon-quit"
